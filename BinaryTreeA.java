2bc6749973e78a2e579fdb63b6eee287978b3424
bini26 <pandeybinod26@gmail.com>
2023-11-16 10:21:42 +0545

    Updated

--- BinaryTreeA.java
+++ BinaryTreeA.java
@@ -209,6 +209,22 @@ public class BinaryTreeA {
 
     }
 
+    public static Node lca2(Node root, int n1, int n2) {
+
+        if (root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right, n1, n2);
+        if (rightLca == null) {
+            return leftLca;
+        }
+        if (leftLca == null) {
+            return rightLca;
+        }
+        return root;
+    }
+
     public static void main(String[] args) {
         /*
          * 1
@@ -240,8 +256,8 @@ public class BinaryTreeA {
         // topView(root);
         // k=2
         // klevel(root,1,k);
-        int n1 = 4, n2 = 6;
-        System.out.println(lca(root, n1, n2).data);
+        int n1 = 4, n2 = 7;
+        System.out.println(lca2(root, n1, n2).data);
 
     }
 }
