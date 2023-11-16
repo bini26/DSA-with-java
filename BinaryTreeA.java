84b174cc7d1da6cb5a17d5c897134c48d55393e7
bini26 <pandeybinod26@gmail.com>
2023-11-16 08:43:12 +0545

    updated Binary Tree

--- BinaryTreeA.java
+++ BinaryTreeA.java
@@ -1,3 +1,7 @@
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.HashMap;
+
 public class BinaryTreeA {
     public static class Node {
         int data;
@@ -41,6 +45,135 @@ public class BinaryTreeA {
 
     }
 
+    public static int diameter2(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDiameter = diameter2(root.left);
+        int rightDiameter = diameter2(root.right);
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        int selfdiameter = leftHeight + rightHeight + 1;
+        int diameter = Math.max(Math.max(leftDiameter, rightDiameter), selfdiameter);
+        return diameter;
+    }
+
+    static class Info {
+        int diameter;
+        int height;
+
+        public Info(int diameter, int height) {
+            this.diameter = diameter;
+            this.height = height;
+        }
+    }
+
+    public static Info diameter(Node root) {
+        if (root == null) {
+            return new Info(0, 0);
+        }
+        Info leftInfo = diameter(root.left);
+        Info rightInfo = diameter(root.right);
+        int finalDiameter = Math.max(Math.max(leftInfo.diameter, rightInfo.diameter),
+                leftInfo.height + rightInfo.height + 1);
+        int finalHeight = Math.max(leftInfo.height, rightInfo.height) + 1;
+        return new Info(finalDiameter, finalHeight);
+
+    }
+
+    public static boolean isIdentical(Node node, Node subNode) {
+        if (node == null && subNode == null) {
+            return true;
+        } else if (node == null || subNode == null || node.data != subNode.data) {
+            return false;
+        }
+        if (!isIdentical(node.left, subNode.left)) {
+            return false;
+        }
+        if (!isIdentical(node.right, subNode.right)) {
+            return false;
+        }
+        return true;
+
+    }
+
+    public static boolean isSubTree(Node root, Node subRoot) {
+        if (root == null) {
+            return false;
+        }
+        if (root.data == subRoot.data) {
+            if (isIdentical(root, subRoot)) {
+                return true;
+            }
+        }
+        boolean leftAns = isSubTree(root.left, subRoot);
+        boolean rightAns = isSubTree(root.right, subRoot);
+
+        return leftAns || rightAns;
+
+    }
+
+    public static class topInfo {
+        Node node;
+        int hd;
+
+        public topInfo(Node node, int hd) {
+            this.node = node;
+            this.hd = hd;
+
+        }
+    }
+
+    public static void topView(Node root) {
+        // Level Order treversal
+        Queue<topInfo> q = new LinkedList<>();
+        HashMap<Integer, Node> map = new HashMap<>();
+        int min = 0;
+        int max = 0;
+        q.add(new topInfo(root, 0));
+        q.add(null);
+        while (!q.isEmpty()) {
+            topInfo curr = q.remove();
+            if (curr == null) {
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                if (!map.containsKey(curr.hd)) {
+                    map.put(curr.hd, curr.node);
+
+                }
+                if (curr.node.left != null) {
+                    q.add(new topInfo(curr.node.left, curr.hd - 1));
+                    min = Math.min(min, curr.hd - 1);
+                }
+                if (curr.node.right != null) {
+                    q.add(new topInfo(curr.node.right, curr.hd + 1));
+                    max = Math.max(max, curr.hd + 1);
+                }
+            }
+        }
+        for (int i = min; i <= max; i++) {
+            System.out.print(map.get(i).data + "  ");
+        }
+        System.out.println();
+    }
+
+    public static void klevel(Node root, int level, int k) {
+        if (root == null) {
+            return;
+        }
+        if (level == k) {
+            System.out.print(root.data + "  ");
+            return;
+        }
+        klevel(root.left, level + 1, k);
+        klevel(root.right, level + 1, k);
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         /*
          * 1
@@ -58,6 +191,20 @@ public class BinaryTreeA {
         root.left.right = new Node(5);
         root.right.left = new Node(6);
         root.right.right = new Node(7);
-        System.out.println(sumNode(root));
+
+        /*
+         * 2
+         * / \
+         * 4 5
+         */
+        // Node subRoot = new Node(2);
+        // subRoot.left = new Node(4);
+        // subRoot.right = new Node(5);
+
+        // System.out.println(isSubTree(root, subRoot));
+        // topView(root);
+        int k = 3;
+        klevel(root, 1, k);
+
     }
 }
