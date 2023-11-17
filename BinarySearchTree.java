db4b720af4cef862a6445bd44585884d43f560c4
bini26 <pandeybinod26@gmail.com>
2023-11-17 20:43:51 +0545

    updated

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -38,6 +38,39 @@ public class BinarySearchTree {
         }
     }
 
+    public static Node findInorderSuccessor(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
+    public static Node delete(Node root, int val) {
+        if (root.data < val) {
+            root.right = delete(root.right, val);
+        } else if (root.data > val) {
+            root.left = delete(root.left, val);
+        } else {
+            // case 1:leaf node/no child
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            // case2:single child
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+            // case 3:two children
+            Node inorderSuccessor = findInorderSuccessor(root.right);
+            root.data = inorderSuccessor.data;
+            root.right = delete(root.right, inorderSuccessor.data);
+
+        }
+        return root;
+
+    }
+
     public static void inorder(Node root) {
         if (root == null) {
             return;
@@ -48,14 +81,17 @@ public class BinarySearchTree {
     }
 
     public static void main(String args[]) {
-        int values[] = { 5, 1, 3, 4, 2, 7 };
+        int values[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
         Node root = null;
         for (int i = 0; i < values.length; i++) {
             root = insert(root, values[i]);
         }
         inorder(root);
         System.out.println();
-        System.out.println(search(root, 8));
+        // System.out.println(search(root, 8));
+        delete(root, 5);
+        System.out.println();
+        inorder(root);
 
     }
 
