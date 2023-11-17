9f162bde9f09d012b88300f0a3d4d1bdb7137ef7
bini26 <pandeybinod26@gmail.com>
2023-11-17 12:08:24 +0545

    updated some files

--- BinaryTreeA.java
+++ BinaryTreeA.java
@@ -270,6 +270,28 @@ public class BinaryTreeA {
         return max + 1;
     }
 
+    public static int transform(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftChild = transform(root.left);
+        int rightChild = transform(root.right);
+        int data = root.data;
+        int newLeft = root.left == null ? 0 : root.left.data;
+        int newRight = root.right == null ? 0 : root.right.data;
+        root.data = newLeft + leftChild + newRight + rightChild;
+        return data;
+    }
+
+    public static void preOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + "  ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
     public static void main(String[] args) {
         /*
          * 1
@@ -303,8 +325,10 @@ public class BinaryTreeA {
         // klevel(root,1,k);
         // int n1 = 4, n2 = 5;
         // System.out.println(minDistanceBtwNodes(root, n1, n2));
-        int n = 4, k = 2;
-        kthAncestor(root, n, k);
+        // int n = 4, k = 2;
+        // kthAncestor(root, n, k);
+        transform(root);
+        preOrder(root);
 
     }
 }
