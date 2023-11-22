6432f061a4b8d001ec131e3390b1f04e427232a8
bini26 <pandeybinod26@gmail.com>
2023-11-22 15:57:36 +0545

    Linked hash map

---
+++ HashMap2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class HashMap2 {
+    public static void main(String[] args) {
+
+        LinkedHashMap<String, Integer> menu = new LinkedHashMap<>();
+        menu.put("Chowmin", 100);
+        menu.put("Chiya", 25);
+        menu.put("MOMO", 120);
+        menu.put("Briyani", 250);
+        System.out.println(menu);
+
+    }
+}
