d920f897408a84dd6d801fafe73d636eec97958f
bini26 <pandeybinod26@gmail.com>
2023-11-24 18:03:30 +0545

    updated

--- Trie2.java
+++ Trie2.java
@@ -56,21 +56,66 @@ public class Trie2 {
         return true;
     }
 
+    public static int countNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < 26; i++) {
+            if (root.children[i] != null) {
+                count += countNodes(root.children[i]);
+            }
+        }
+        return count + 1;
+    }
+
+    public static String ans = "";
+
+    public static void longestWord(Node root, StringBuilder temp) {
+        if (root == null) {
+            return;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (root.children[i] != null && root.children[i].eow == true) {
+                char ch = (char) (i + 'a');
+                temp.append(ch);
+                if (temp.length() > ans.length()) {
+                    ans = temp.toString();
+                }
+                longestWord(root.children[i], temp);
+                temp.deleteCharAt(temp.length() - 1);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        String arr[] = { "zebra", "dog", "duck", "dove" };
-        for (int i = 0; i < arr.length; i++) {
-            insert(arr[i]);
+        // String arr[] = { "zebra", "dog", "duck", "dove" };
+        // for (int i = 0; i < arr.length; i++) {
+        // insert(arr[i]);
+        // }
+        // root.freq = -1;
+        // findPrefix(root, "");
+        // String word[] = { "apple", "app", "mango", "man", "women" };
+        // String prefix = "app";
+        // String prefix2 = "moon";
+        // for (int i = 0; i < word.length; i++) {
+        // insert(word[i]);
+        // }
+        // System.out.println(startsWith(prefix));
+        // System.out.println(startsWith(prefix2));
+
+        // String str = "ababa";
+        // for (int i = 0; i < str.length(); i++) {
+        // String suffix = str.substring(i);
+        // insert(suffix);
+        // }
+        // System.out.println(countNodes(root));
+        String words[] = { "a", "banana", "app", "appl", "ap", "apply", "apple" };
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
         }
-        root.freq = -1;
-        findPrefix(root, "");
-        String word[] = { "apple", "app", "mango", "man", "women" };
-        String prefix = "app";
-        String prefix2 = "moon";
-        for (int i = 0; i < word.length; i++) {
-            insert(word[i]);
-        }
-        System.out.println(startsWith(prefix));
-        System.out.println(startsWith(prefix2));
+        longestWord(root, new StringBuilder());
+        System.out.println(ans);
     }
 
 }
