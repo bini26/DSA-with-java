6bb6dc21ed8bcd50860cf35c95a4ab641fdcaffb
bini26 <pandeybinod26@gmail.com>
2023-11-24 16:15:28 +0545

    updated

--- Trie.java
+++ Trie.java
@@ -3,7 +3,7 @@ public class Trie {
         Node children[] = new Node[26];
         boolean eow = false;
 
-        Node() {
+        public Node() {
             for (int i = 0; i < 26; i++) {
                 children[i] = null;
             }
@@ -24,11 +24,47 @@ public class Trie {
         curr.eow = true;
     }
 
-    public static void main(String[] args) {
-        String words[] = { "the", "a", "there", "any", "thee" };
-        for (int i = 0; i < words.length; i++) {
-            insert(words[i]);
+    public static boolean search(String key) {
+        Node curr = root;
+        for (int level = 0; level < key.length(); level++) {
+            int idx = key.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
         }
+        return curr.eow = true;
+    }
+
+    public static boolean wordBreak(String key) {
+        if (key.length() == 0) {
+            return true;
+        }
+        for (int i = 1; i <= key.length(); i++) {
+            // substring(0,1)
+
+            if (search(key.substring(0, i)) &&
+                    wordBreak(key.substring(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        // String words[] = { "the", "a", "there", "any", "thee" };
+        // for (int i = 0; i < words.length; i++) {
+        // insert(words[i]);
+        // }
+        // System.out.println(search("thee"));
+
+        String arr[] = { "i", "like", "sam", "samsung", "mobile", "ice" };
+        for (int i = 0; i < arr.length; i++) {
+            insert(arr[i]);
+        }
+        String key = "ilikesungm";
+
+        System.out.println(wordBreak(key));
     }
 
 }
