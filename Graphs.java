7aaf54d75a9cd82b934fc6389aa590c96c0e266a
bini26 <pandeybinod26@gmail.com>
2023-11-25 15:31:58 +0545

    updated

--- Graphs.java
+++ Graphs.java
@@ -13,54 +13,67 @@ public class Graphs {
         }
     }
 
-    public static void main(String[] args) {
-        /*
-         * (5)
-         * 0..................1
-         * (1) / \ (3)
-         * / \
-         * 2.........3
-         * (2) |
-         * |
-         * 4
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         */
-
-        int vertices = 5;
-        ArrayList<Edge>[] graph = new ArrayList[vertices];
-        for (int i = 0; i < vertices; i++) {
+    public static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
         // 0 vertices
-        graph[0].add(new Edge(0, 1, 5));
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
         // 1 vertces
-        graph[1].add(new Edge(1, 0, 5));
-        graph[1].add(new Edge(1, 2, 1));
-        graph[1].add(new Edge(1, 3, 3));
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
         // 2 vertices
-        graph[2].add(new Edge(2, 1, 1));
-        graph[2].add(new Edge(2, 3, 1));
-        graph[2].add(new Edge(2, 4, 4));
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
         // 3 vertices
-        graph[3].add(new Edge(3, 1, 3));
-        graph[3].add(new Edge(3, 2, 1));
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(4, 2, 1));
+        graph[3].add(new Edge(3, 5, 1));
         // 4 vertices
-        graph[4].add(new Edge(4, 2, 2));
-        // 2's neighbour
-        for (int i = 0; i < graph[2].size(); i++) {
-            Edge e = graph[2].get(i);
-            System.out.print("Source -> " + e.src + "  Destination -> " + e.dest + "  Weight->  " + e.weight);
-            System.out.println();
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 2));
+        // 5 vertices
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
+        // 6 vertices
+        graph[6].add(new Edge(6, 5, 1));
+    }
+
+    public static void bfs(ArrayList<Edge>[] graph) {
+        Queue<Integer> q = new java.util.LinkedList<>();
+        boolean visit[] = new boolean[graph.length];
+        q.add(0);
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+
+            if (!visit[curr]) {
+                System.out.print(curr + "  ");
+                visit[curr] = true;
+                for (int i = 0; i < graph[curr].size(); i++) {
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
         }
+    }
+
+    public static void main(String[] args) {
+
+        int vertices = 7;
+        ArrayList<Edge>[] graph = new ArrayList[vertices];
+        createGraph(graph);
+
+        // 2's neighbour
+        // for (int i = 0; i < graph[2].size(); i++) {
+        // Edge e = graph[2].get(i);
+        // System.out.print("Source -> " + e.src + " Destination -> " + e.dest + "
+        // Weight-> " + e.weight);
+        // System.out.println();
+        // }
+        bfs(graph);
 
     }
 
