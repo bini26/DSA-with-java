05ec7bede4013f7d60dbf027596b43555cc9affe
bini26 <pandeybinod26@gmail.com>
2023-11-26 17:10:37 +0545

    updated

--- Graph2.java
+++ Graph2.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.LinkedList;
 
 public class Graph2 {
     static class Edge {
@@ -17,20 +18,20 @@ public class Graph2 {
         }
         // 0 vertices
         graph[0].add(new Edge(0, 1));
-        // graph[0].add(new Edge(0, 2));
-        graph[0].add(new Edge(0, 3));
+        graph[0].add(new Edge(0, 2));
+        // graph[0].add(new Edge(0, 3));
         // 1 vertces
         graph[1].add(new Edge(1, 0));
-        graph[1].add(new Edge(1, 2));
+        graph[1].add(new Edge(1, 3));
         // 2 vertices
-        // graph[2].add(new Edge(2, 0));
-        graph[2].add(new Edge(2, 1));
+        graph[2].add(new Edge(2, 0));
+        // graph[2].add(new Edge(2, 4));
         // 3 vertices
-        graph[3].add(new Edge(3, 0));
-        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 2));
+        graph[3].add(new Edge(3, 1));
         // 4 vertices
 
-        graph[4].add(new Edge(4, 3));
+        // graph[4].add(new Edge(4, 3));
 
     }
 
@@ -64,11 +65,41 @@ public class Graph2 {
         return false;
     }
 
+    public static boolean isBipartite(ArrayList<Edge>[] graph) {
+        int color[] = new int[graph.length];
+        for (int i = 0; i < color.length; i++) {
+            color[i] = -1;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < graph.length; i++) {
+            if (color[i] == -1) {
+                q.add(i);
+                color[i] = 0;
+                while (!q.isEmpty()) {
+                    int curr = q.remove();
+                    for (int j = 0; j < graph[curr].size(); j++) {
+                        Edge e = graph[curr].get(j);
+                        if (color[e.dest] == -1) {
+                            int nextColor = color[curr] == 0 ? 1 : 0;
+                            color[e.dest] = nextColor;
+                            q.add(e.dest);
+                        } else if (color[e.dest] == color[curr]) {
+                            return false;
+                        }
+                    }
+
+                }
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        int v = 5;
+        int v = 4;
         ArrayList<Edge> graph[] = new ArrayList[v];
         createGraph(graph);
-        System.out.println(detectCycle(graph));
+        // System.out.println(detectCycle(graph));
 
+        System.out.println(isBipartite(graph));
     }
 }
