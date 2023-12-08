fcbf7fcc16aed6abb0ae66332e448feb80e538af
bini26 <pandeybinod26@gmail.com>
2023-12-08 07:36:27 +0545

    updated dp file

--- KnapsackDP.java
+++ KnapsackDP.java
@@ -85,6 +85,27 @@ public class KnapsackDP {
         return dp[n][target];
     }
 
+    public static int UnboundedKnapsack(int val[], int wt[], int W) {
+        int n = val.length;
+        int dp[][] = new int[n + 1][W + 1];
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j < W + 1; j++) {
+            dp[0][j] = 0;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < W + 1; j++) {
+                if (wt[i - 1] <= j) {// valid
+                    dp[i][j] = Math.max(val[i - 1] + dp[i][j - wt[i - 1]], dp[i - 1][j]);
+                } else {// invalid
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][W];
+    }
+
     public static void main(String[] args) {
         int val[] = { 15, 14, 10, 45, 30 };
         int wt[] = { 2, 5, 1, 3, 4 };
@@ -100,6 +121,7 @@ public class KnapsackDP {
         // System.out.println(knapsackTabulation(val, wt, W));
         int number[] = { 4, 2, 7, 1, 3 };
         int targetSum = 10;
-        System.out.println(targetSumTabulation(number, targetSum));
+        // System.out.println(targetSumTabulation(number, targetSum));
+        System.out.println(UnboundedKnapsack(val, wt, W));
     }
 }
---
+++ UnboundedKnapsack.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class UnboundedKnapsack {
+
+    public static void main(String[] args) {
+
+    }
+
+}
